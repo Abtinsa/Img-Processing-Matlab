cb54a0258664e78771946dbfcc31dac9091d6a79
Abtin Aghasadeghi <abtinsa@Abtins-MacBook-Pro.local>
2025-03-03 17:17:23 +0330

    Initial commit - Flash-Floods-Swamp-Iran files

---
+++ calculateArea.m
@@ -0,0 +1,12 @@
+function results = calculateArea(I)
+% Segment
+BW = createMask(I);
+% Area in pixels
+props = regionprops("table",BW,"Area");
+areaPixels = sum(props.Area);
+% Area in km^2
+px2km = (70/100)^2;
+areaKmSq = round(areaPixels*px2km);
+% Format into structure
+results.BW = BW;
+results.areaKmSq = areaKmSq;
